d1340e9998509e20beac157c71fb5a64013d3f4e
ZapAnton <wc3ft2@gmail.com>
2019-02-12 19:13:24 +0300

    Added basic command line arguments processing: REPL and file reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+use std::{
+    env, fs,
+    io::{stdin, stdout, Write},
+    path::Path,
+    process,
+};
+
+fn run(source: &str) {
+    println!("{}", source);
+}
+
+fn get_user_input() -> String {
+    let mut buffer = String::new();
+
+    stdout().flush().unwrap();
+
+    stdin().read_line(&mut buffer).unwrap();
+
+    buffer.trim().to_string()
+}
+
+fn run_prompt() {
+    loop {
+        print!("> ");
+
+        let user_input = get_user_input();
+
+        run(&user_input);
+    }
+}
+
+fn run_file(file_arg: &str) {
+    let file_path = Path::new(file_arg);
+
+    let file_content = fs::read_to_string(&file_path).unwrap();
+
+    run(&file_content);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args();
+
+    match args.len() {
+        1 => run_prompt(),
+        2 => run_file(&args.last().unwrap()),
+        _ => {
+            println!("Usage: locl [script]");
+
+            process::exit(64);
+        }
+    }
 }
